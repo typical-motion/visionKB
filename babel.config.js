653b01ff0ce0fd1fe67b7fbad5367dbd74d5c0ca
duskkkrua <yyb2448@163.com>
2022-12-27 14:58:30 +0800

    需要自己操作，多加体验

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+const math = require('remark-math');
+const katex = require('rehype-katex');
+
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+  remarkPlugins: [math],
+  rehypePlugins: [katex],
 };
