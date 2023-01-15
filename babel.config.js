095ca73ea610415f4810bd115e1d54b875bc16d8
duskkkrua <yyb2448@163.com>
2023-01-15 12:10:26 +0800

    修改部分报错

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,5 @@
-const math = require('remark-math');
-const katex = require('rehype-katex');
+
 
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
-  remarkPlugins: [math],
-  rehypePlugins: [katex],
 };
